7260c33df256e57b296da11981d460acff729086
Diego Coelho <diegocoelhoaprova@gmail.com>
2026-07-06 20:00:16 -0300

    feat: create Header and NavLink components, implement layout structure in AppLayout

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,24 @@
+import { Separator as SeparatorPrimitive } from '@base-ui/react/separator';
+
+import { cn } from '@/lib/utils';
+
+function Separator({
+  className,
+  orientation = 'horizontal',
+  ...props
+}: SeparatorPrimitive.Props) {
+  return (
+    <SeparatorPrimitive
+      className={cn(
+        'shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+        className
+      )}
+      data-slot="separator"
+      orientation={orientation}
+      {...props}
+    />
+  );
+}
+
+export { Separator };
